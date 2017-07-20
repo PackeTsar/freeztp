f4d1c9851b0237235c97a451251440c6e30f3583
John W Kerns <jkerns@packetsar.com>
2017-07-19 17:20:39 -0700

    More config working

--- fztp.py
+++ fztp.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+
+'''
+To Do:
+	- Build installer (run as service)
+	- Build service control
+	- Build autocomplete
+
+
+
+
+NEXT: Get app to find config file in /etc/ztp or local
+
+
+
+'''
 
 configfile = "fztp.cfg"
 
@@ -176,6 +192,8 @@ import commands
 class config_manager:
 	def __init__(self):
 		self._publish()
+	def _find_config(self):
+		pass
 	def _file_exists(self, filepath):
 		checkdata = commands.getstatusoutput("ls " + filepath)
 		exists = True
@@ -204,20 +222,78 @@ class config_manager:
 		f = open(configfile, "w")
 		self.rawconfig = f.write(self.rawconfig)
 		f.close()
-		print("done!")
-	def set(self, setting, value):
+	def set(self, args):
+		setting = args[2]
+		value = args[3]
 		exceptions = ["finaltemplate", "keyvalstore", "starttemplate"]
 		if setting in exceptions:
 			print("Cannot configure this way")
+		elif "template" in setting:
+			print("Enter each line of the template ending with '%s' on a line by itself" % value)
+			newinitial = self.multilineinput(value)
+			if setting == "initial-template":
+				self.running["starttemplate"] = newinitial
+			elif setting == "final-template":
+				self.running["finaltemplate"] = newinitial
+			self.save()
+		elif setting == "keystore":
+			self.set_keystore(args[3], args[4], args[5] )
 		else:
 			if setting in list(self.running):
 				self.running[setting] = value
 				self.save()
-				print("OK")
-
-
-
-
+			else:
+				print("Unknown Setting!")
+	def clear(self, args):
+		setting = args[2]
+		if setting == "keystore":
+			iden = args[3]
+			key = args[4]
+			if iden not in list(self.running["keyvalstore"]):
+				print("ID does not exist in keystore: %s" % iden)
+			else:
+				if key == "all":
+					del self.running["keyvalstore"][iden]
+				else:
+					if key not in list(self.running["keyvalstore"][iden]):
+						print("Key does not exist under ID %s: %s" % (iden, key))
+					else:
+						del self.running["keyvalstore"][iden][key]
+			self.save()
+	def multilineinput(self, ending):
+		result = ""
+		for line in iter(raw_input, ending):
+			result += line+"\n"
+		return result[0:len(result)-1]
+	def set_keystore(self, iden, keyword, value):
+		if iden in list(self.running["keyvalstore"]):
+			self.running["keyvalstore"][iden].update({keyword: value})
+		else:
+			self.running["keyvalstore"].update({iden: {keyword: value}})
+		self.save()
+	def show_config(self):
+		cmdlist = []
+		simplevals = ["finalsuffix", "startfilename", "startsnmpcommunity", "startsnmpoid"]
+		for each in simplevals:
+			cmdlist.append("ztp set %s %s" % (each, self.running[each]))
+		itemp = "ztp set initial-template ^\n%s\n^" % self.running["starttemplate"]
+		ftemp = "ztp set final-template ^\n%s\n^" % self.running["finaltemplate"]
+		keylist = []
+		for iden in self.running["keyvalstore"]:
+			for key in self.running["keyvalstore"][iden]:
+				value = self.running["keyvalstore"][iden][key]
+				keylist.append("ztp set keystore %s %s %s" % (iden, key, value))
+		config = "!\n!\n!\n"
+		for cmd in cmdlist:
+			config += cmd + "\n!\n"
+		config += "!\n!\n!\n"
+		for cmd in keylist:
+			config += cmd + "\n!\n"
+		config += "\n#######################################################\n"
+		config += itemp
+		config += "\n#######################################################\n"
+		config += ftemp
+		print(config)
 
 import sys
 
@@ -237,11 +313,66 @@ def interpreter():
 	if arguments == "run":
 		start_tftp()
 		config = config_factory()
-	elif arguments == "test":
-		config.finalsuffix = "-confg"
-		config.save()
-	elif sys.argv[1] == "set":
-		config.set(sys.argv[2], sys.argv[3])
+	##### SHOW #####
+	elif arguments == "show initial-template":
+		print(config.running["starttemplate"])
+	elif arguments == "show final-template":
+		print(config.running["finaltemplate"])
+	elif arguments == "show config":
+		config.show_config()
+	##### SET #####
+	elif arguments == "set":
+		print " - set finalsuffix <value>                        |  Set the file name suffix used by target when requesting the final config"
+		print " - set startfilename <value>                      |  Set the file name used by the target during the initial config request"
+		print " - set startsnmpcommunity <value>                 |  Set the SNMP community you want to use for target ID identification"
+		print " - set startsnmpoid <value>                       |  Set the SNMP OID to use to pull the target ID during identification"
+		print " - set initial-template (<end char>)              |  Set the initial configuration j2 template used to prep target for identification"
+		print " - set final-template (<end char>)                |  Set the final configuration j2 template pushed to host after discovery/identification"
+		print " - set keystore <id> <keyword> <value>            |  Set the final configuration template pushed to host after discovery/identification"
+	elif arguments == "set finalsuffix":
+		print " - set finalsuffix <value>                        |  Set the file name suffix used by target when requesting the final config"
+	elif arguments == "set startfilename":
+		print " - set startfilename <value>                      |  Set the file name used by the target during the initial config request"
+	elif arguments == "set startsnmpcommunity":
+		print " - set startsnmpcommunity <value>                 |  Set the SNMP community you want to use for target ID identification"
+	elif arguments == "set startsnmpoid":
+		print " - set startsnmpoid <value>                       |  Set the SNMP OID to use to pull the target ID during identification"
+	elif arguments[:3] == "set" and len(sys.argv) >= 4:
+		config.set(sys.argv)
+	##### CLEAR #####
+	elif arguments == "clear":
+		print " - clear keystore <id> (all|<keyword>)            |  Delete information from the keystore"
+	elif arguments[:14] == "clear keystore" and len(sys.argv) >= 5:
+		config.clear(sys.argv)
+	else:
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print "                     ARGUMENTS                    |                                  DESCRIPTIONS"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - run                                            |  Run the ZTP main program in shell mode begin listening for TFTP requests"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - install                                        |  Run the ZTP installer"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - show config                                    |  Show the current ZTP configuration"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - set finalsuffix <value>                        |  Set the file name suffix used by target when requesting the final config"
+		print " - set startfilename <value>                      |  Set the file name used by the target during the initial config request"
+		print " - set startsnmpcommunity <value>                 |  Set the SNMP community you want to use for target ID identification"
+		print " - set startsnmpoid <value>                       |  Set the SNMP OID to use to pull the target ID during identification"
+		print "------------------------------------------------"
+		print " - set initial-template (<end char>)              |  Set the initial configuration j2 template used to prep target for identification"
+		print " - set final-template (<end char>)                |  Set the final configuration j2 template pushed to host after discovery/identification"
+		print "------------------------------------------------"
+		print " - set keystore <id> <keyword> <value>            |  Set the final configuration template pushed to host after discovery/identification"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - clear keystore <id> (all|<keyword>)            |  Delete information from the keystore"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+		print " - version                                        |  Show the current version of ZTP"
+		print "-------------------------------------------------------------------------------------------------------------------------------"
+
+
+
+
+
 
 
 
@@ -249,5 +380,5 @@ def interpreter():
 
 if __name__ == "__main__":
 	interpreter()
-	
+
 
